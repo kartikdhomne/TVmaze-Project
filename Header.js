3391717a1750ceac1c9aaf180e94ed0b77c93992
kartikdhomne <92445043+kartikdhomne@users.noreply.github.com>
2022-01-12 14:21:29 +0530

    Update Header.js

--- Header.js
+++ Header.js
@@ -8,9 +8,9 @@ const Header = () => {
         <div className='ABC'>
         <h1>TVmaze</h1>
          <h3>Search your favourite shows</h3>
-        <input type="radio" name="Actor" required/>Actor 
+        <input type="radio" name="Category" value="Actor" required/>Actor 
 
-        <input type="radio" name="Shows" required/>Shows <br /><br />
+        <input type="radio" name="Category" value="Shows" required/>Shows <br /><br />
 
         <SearchBox/>
         </div>
